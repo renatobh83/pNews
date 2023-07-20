a6a19986591af54ea93d5797ba8376fffb9b45e1
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-20 15:40:16 -0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from 'react'
+import {DataNews } from "../components/DataNews"
+
+import Loading from "./loading"
+
+async function getData() {
+  const res = await fetch('https://puppeteer-title.onrender.com/news', { next: { revalidate: 120 } })
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+ 
+
+
+
+export default async function Home() {
+  const notocias = await getData()
+   return (
+     <main className="min-h-screen sm:px-10 px-2 mb-4">
+        <Suspense fallback={<Loading/>}>
+           <DataNews data={notocias}/>
+        </Suspense>
+    </main>
+  )
+}
