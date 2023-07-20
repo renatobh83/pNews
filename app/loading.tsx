5b5958abcd66383690c879db27383e5870b047d8
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-20 20:39:29 -0300

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,6 @@
 export default function Loading() {
   return (
-<div className="bg-indigo-500" >
-  <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24"></svg>
+    <div>
       Carregando...
     </div>
     )
