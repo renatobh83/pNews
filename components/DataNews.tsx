d46e5e86da4b0f8c1cb163279b6a701844b9e749
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-20 17:25:43 -0300

    Update DataNews.tsx

--- components/DataNews.tsx
+++ components/DataNews.tsx
@@ -1,19 +1,14 @@
 "use client";
-export function DataNews( { data }: { data: string[] }) {
+export function DataNews( { data }: { data: [{jornal: string, noticia:string}] }) {
   return (
     <>
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-    <div>Noticias</div>
-    <div className="flex gap-3">
-
-    </div>  
-    </div>
-    <div className="md:flex grid gap-2 flex-wrap justify-center">  
+    <div className="md:flex grid gap-2 flex-wrap justify-center mt-2">  
     {data?.map(valor  => (
           <div key={valor.noticia} className="max-w-sm px-2 sm:py-6 py-2 basis-30 grow
                           bg-white border border-gray-200  rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <div className="flex items-center space-x-4">
-                <p className="text-lg font-medium text-gray-900 dark:text-white"><span className="uppercase text-red-600">{valor.jornal}</span> - {valor.noticia}</p>
+                <p className="text-lg tracking-wide font-medium text-gray-900 dark:text-white"><span className="uppercase text-gray-900 dark:text-white">
+                  {valor.jornal}</span> - {valor.noticia}</p>
               </div>
             </div>
       ) as any)
