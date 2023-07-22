80b09dab78183e39b300f2fbb9f8cc102e2d35f3
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-22 18:40:32 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from 'react'
 import { DataNews } from "../components/DataNews"
-
+export const revalidate = 10
 export const runtime = 'edge' // 'nodejs' (default) | 'edge'
 async function getData() {
-  const res = await fetch('https://puppeteer-title.onrender.com/news', { cache: 'no-store' })
+  // const res = await fetch('https://puppeteer-title.onrender.com/news', { cache: 'no-store' })
+   const res = await fetch('https://node-news.vercel.app/news')
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
