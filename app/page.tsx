c7a1d13ec8a7a697740de4ae12fc2d357234301d
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-22 22:24:35 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
+import {DataNews } from "../components/DataNews"
 
-import { DataNews } from "../components/DataNews"
-export const runtime = 'edge' // 'nodejs' (default) | 'edge'
-async function getData() {
-  // const res = await fetch('https://puppeteer-title.onrender.com/news', { cache: 'no-store' })
-   const res = await fetch('https://node-news.vercel.app/news',{ cache: 'no-store' })
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-  return res.json()
-}
- 
-export default async function Home() {
-  const noticias = await getData()
+export default function Home(params) {
    return (
-     <main className="min-h-screen sm:px-10 px-2 mb-4">
-          <DataNews data={noticias}/>
+     <main className="sm:px-10 px-2 mb-4 md:text-xl text-sm">
+           <DataNews params={params}/>
     </main>
   )
 }
