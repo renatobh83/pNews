1fdd10f36f208f8ff4db565e89a8f46113a10af3
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-22 22:26:53 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import {DataNews } from "../components/DataNews"
 
-export default function Home(params) {
+export default function Home() {
    return (
      <main className="sm:px-10 px-2 mb-4 md:text-xl text-sm">
            <DataNews/>
