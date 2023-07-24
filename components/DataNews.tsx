9cc9e6839c12ad6de75e45ffe58d844691fb5e88
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-24 13:17:45 -0300

    Update DataNews.tsx

--- components/DataNews.tsx
+++ components/DataNews.tsx
@@ -6,7 +6,7 @@ const fetcher = url => fetch(url, { cache: 'no-store' }).then(r => r.json())
 
 export const runtime = 'edge' 
 
-export async function DataNews({params} : string) {
+export async function DataNews({params}) {
 
 const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_BASE_URL}/news?url=${params}`, fetcher, { refreshInterval: 40000, refreshWhenHidden: true })
   return (
