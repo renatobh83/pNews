2cd92292ff4ff7bea2cb166a147dd8e3dab8353b
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-24 16:13:10 -0300

    Update Jornal.tsx

--- components/Jornal.tsx
+++ components/Jornal.tsx
@@ -1,6 +1,6 @@
 const links = ["https://valor.globo.com/ultimas-noticias/&classe=feed-post-link gui-color-primary&jornal=Valor",
                 "https://www1.folha.uol.com.br/ultimas-noticias/&classe=c-headline__title&jornal=Folha de Sp",
-	      "https://br.investing.com/news/latest-news&classe=title&jornal=Investing",
+	      "https://economia.uol.com.br/ultimas/&classe=thumb-title title-xsmall title-lg-small&jornal=UOL",
 	      "https://www.estadao.com.br/ultimas/&classe=headline&jornal=Estadao"]
 export const Jornal = () => {
 	return links
