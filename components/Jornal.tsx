954535c77f4610c9ab68ad1cc67dc7c8753667d3
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-24 13:13:53 -0300

    Add files via upload

---
+++ components/Jornal.tsx
@@ -0,0 +1,10 @@
+const links = ["https://valor.globo.com/ultimas-noticias/&classe=feed-post-link gui-color-primary&jornal=Valor",
+                "https://www1.folha.uol.com.br/ultimas-noticias/&classe=c-headline__title&jornal=Folha de Sp"]
+export const Jornal = () => {
+	return links
+}
+
+export const AddJornal = () => {
+	const link = "https://www.estadao.com.br/ultimas/&classe=headline&jornal=Estadao"
+	links.push(link)
+}
