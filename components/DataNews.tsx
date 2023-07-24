aa6c2adf22b53d72cd75de2b1773f1c095d17e4b
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-24 13:12:42 -0300

    Update DataNews.tsx

--- components/DataNews.tsx
+++ components/DataNews.tsx
@@ -1,21 +1,17 @@
 "use client"
 import useSWR from 'swr'
 
+
 const fetcher = url => fetch(url, { cache: 'no-store' }).then(r => r.json())
+
 export const runtime = 'edge' 
 
-export async function DataNews() {
-    const { data, error, isLoading } = useSWR("https://node-news.vercel.app/news", fetcher, { refreshInterval: 40000, refreshWhenHidden: true })
+export async function DataNews({params} : string) {
 
-
-  if(isLoading) {
-    return <div className="flex justify-center"><p>Loading...</p></div>
-  }
-
+const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_BASE_URL}/news?url=${params}`, fetcher, { refreshInterval: 40000, refreshWhenHidden: true })
   return (
     <>
     <div >  
-          {/*<div className="flex justify-between"><Reload /></div>*/}
     {data?.map(valor  => (
           <div key={valor.noticia} className="p-2">
               <div>
