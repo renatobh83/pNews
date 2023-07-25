60e8faa397763f0604c69361de7a6becf9ab99fe
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-25 10:21:05 -0300

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], weight: '400' })
+
+export const metadata: Metadata = {
+  title: 'News App',
+  description: 'App News',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
