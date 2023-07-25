e39eb2e2e68a4a04bde90bdde2c4d7435cdadd01
Renato Lucio de Mendonça <neo_rlm@hotmail.com>
2023-07-25 12:51:51 -0300

    Update Jornal.tsx

--- components/Jornal.tsx
+++ components/Jornal.tsx
@@ -1,10 +1,12 @@
-const links = ["https://valor.globo.com/ultimas-noticias/&classe=h2.feed-post-link&jornal=Valor",
+const links = [
+"https://valor.globo.com/ultimas-noticias/&classe=h2.feed-post-link&jornal=Valor",
 "https://www1.folha.uol.com.br/ultimas-noticias/&classe=h2.c-headline__title&jornal=Folha de Sp",
 "https://www.estadao.com.br/ultimas/&classe=h3.headline&jornal=Estadao",
 "https://br.investing.com/news/&classe=div.largeTitle>article>.textDiv>a&jornal=Investig",
 "https://economia.uol.com.br/ultimas/&classe=h3.thumb-title>article>.textDiv>a&jornal=Uol",
 "https://www.bomdiamercado.com.br/noticias/&classe=h4.title&jornal=BDM",
-"http://broadcast.com.br/cadernos/financeiro/&classe=.materia>h3>a&jornal=Broadcast"]
+"http://broadcast.com.br/cadernos/financeiro/&classe=.materia>h3>a&jornal=Broadcast",
+"https://www.cnbc.com/world/?region=world&classe=.RiverHeadline-headline&jornal=CNBC"]
 export const Jornal = () => {
 	return links
 }
